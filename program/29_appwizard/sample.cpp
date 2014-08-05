db94db66fb36574f6aa38a1a223e6cee1a0207a6
Takashi Imagire <imagire@gmail.com>
2014-08-05 15:19:26 +0900

    1st commit

---
+++ program/29_appwizard/sample.cpp
@@ -0,0 +1,475 @@
+//-----------------------------------------------------------------------------
+// File: sample.cpp
+//
+// Desc: DirectX window application created by the DirectX AppWizard
+//-----------------------------------------------------------------------------
+#define STRICT
+#include <windows.h>
+#include <basetsd.h>
+#include <math.h>
+#include <stdio.h>
+#include <D3DX8.h>
+#include <DXErr8.h>
+#include <tchar.h>
+#include "D3DApp.h"
+#include "D3DFont.h"
+#include "D3DFile.h"
+#include "D3DUtil.h"
+#include "DXUtil.h"
+#include "resource.h"
+#include "sample.h"
+
+
+
+//-----------------------------------------------------------------------------
+// Global access to the app (needed for the global WndProc())
+//-----------------------------------------------------------------------------
+CMyD3DApplication* g_pApp  = NULL;
+HINSTANCE          g_hInst = NULL;
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: WinMain()
+// Desc: Entry point to the program. Initializes everything, and goes into a
+//       message-processing loop. Idle time is used to render the scene.
+//-----------------------------------------------------------------------------
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR, INT )
+{
+    CMyD3DApplication d3dApp;
+
+    g_pApp  = &d3dApp;
+    g_hInst = hInst;
+
+    if( FAILED( d3dApp.Create( hInst ) ) )
+        return 0;
+
+    return d3dApp.Run();
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: CMyD3DApplication()
+// Desc: Application constructor. Sets attributes for the app.
+//-----------------------------------------------------------------------------
+CMyD3DApplication::CMyD3DApplication()
+{
+    m_dwCreationWidth           = 500;
+    m_dwCreationHeight          = 375;
+    m_strWindowTitle            = TEXT( "sample" );
+    m_bUseDepthBuffer           = TRUE;
+
+    // Create a D3D font using d3dfont.cpp
+    m_pFont                     = new CD3DFont( _T("Arial"), 12, D3DFONT_BOLD );
+    m_bLoadingApp               = TRUE;
+    m_pD3DXMesh                 = NULL;
+
+    ZeroMemory( &m_UserInput, sizeof(m_UserInput) );
+    m_fWorldRotX                = 0.0f;
+    m_fWorldRotY                = 0.0f;
+
+    // Read settings from registry
+    ReadSettings();
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: OneTimeSceneInit()
+// Desc: Called during initial app startup, this function performs all the
+//       permanent initialization.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::OneTimeSceneInit()
+{
+    // TODO: perform one time initialization
+
+    // Drawing loading status message until app finishes loading
+    SendMessage( m_hWnd, WM_PAINT, 0, 0 );
+
+    m_bLoadingApp = FALSE;
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: ReadSettings()
+// Desc: Read the app settings from the registry
+//-----------------------------------------------------------------------------
+VOID CMyD3DApplication::ReadSettings()
+{
+    HKEY hkey;
+    if( ERROR_SUCCESS == RegCreateKeyEx( HKEY_CURRENT_USER, DXAPP_KEY, 
+        0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hkey, NULL ) )
+    {
+        // TODO: change as needed
+
+        // Read the stored window width/height.  This is just an example,
+        // of how to use DXUtil_Read*() functions.
+        DXUtil_ReadIntRegKey( hkey, TEXT("Width"), &m_dwCreationWidth, m_dwCreationWidth );
+        DXUtil_ReadIntRegKey( hkey, TEXT("Height"), &m_dwCreationHeight, m_dwCreationHeight );
+
+        RegCloseKey( hkey );
+    }
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: WriteSettings()
+// Desc: Write the app settings to the registry
+//-----------------------------------------------------------------------------
+VOID CMyD3DApplication::WriteSettings()
+{
+    HKEY hkey;
+    DWORD dwType = REG_DWORD;
+    DWORD dwLength = sizeof(DWORD);
+
+    if( ERROR_SUCCESS == RegCreateKeyEx( HKEY_CURRENT_USER, DXAPP_KEY, 
+        0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hkey, NULL ) )
+    {
+        // TODO: change as needed
+
+        // Write the window width/height.  This is just an example,
+        // of how to use DXUtil_Write*() functions.
+        DXUtil_WriteIntRegKey( hkey, TEXT("Width"), m_rcWindowClient.right );
+        DXUtil_WriteIntRegKey( hkey, TEXT("Height"), m_rcWindowClient.bottom );
+
+        RegCloseKey( hkey );
+    }
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: ConfirmDevice()
+// Desc: Called during device initialization, this code checks the display device
+//       for some minimum set of capabilities
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::ConfirmDevice( D3DCAPS8* pCaps, DWORD dwBehavior,
+                                          D3DFORMAT Format )
+{
+    BOOL bCapsAcceptable;
+
+    // TODO: Perform checks to see if these display caps are acceptable.
+    bCapsAcceptable = TRUE;
+
+    if( bCapsAcceptable )         
+        return S_OK;
+    else
+        return E_FAIL;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: InitDeviceObjects()
+// Desc: Initialize scene objects.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::InitDeviceObjects()
+{
+    // TODO: create device objects
+
+    HRESULT hr;
+
+    // Init the font
+    m_pFont->InitDeviceObjects( m_pd3dDevice );
+
+    // Create a teapot mesh using D3DX
+    if( FAILED( hr = D3DXCreateTeapot( m_pd3dDevice, &m_pD3DXMesh, NULL ) ) )
+        return DXTRACE_ERR_NOMSGBOX( "D3DXCreateTeapot", hr );
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: RestoreDeviceObjects()
+// Desc: Restores scene objects.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::RestoreDeviceObjects()
+{
+    // TODO: setup render states
+
+    // Setup a material
+    D3DMATERIAL8 mtrl;
+    D3DUtil_InitMaterial( mtrl, 1.0f, 0.0f, 0.0f );
+    m_pd3dDevice->SetMaterial( &mtrl );
+
+    // Set up the textures
+    m_pd3dDevice->SetTextureStageState( 0, D3DTSS_COLOROP,   D3DTOP_MODULATE );
+    m_pd3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
+    m_pd3dDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
+    m_pd3dDevice->SetTextureStageState( 0, D3DTSS_MINFILTER, D3DTEXF_LINEAR );
+    m_pd3dDevice->SetTextureStageState( 0, D3DTSS_MAGFILTER, D3DTEXF_LINEAR );
+
+    // Set miscellaneous render states
+    m_pd3dDevice->SetRenderState( D3DRS_DITHERENABLE,   FALSE );
+    m_pd3dDevice->SetRenderState( D3DRS_SPECULARENABLE, FALSE );
+    m_pd3dDevice->SetRenderState( D3DRS_ZENABLE,        TRUE );
+    m_pd3dDevice->SetRenderState( D3DRS_AMBIENT,        0x000F0F0F );
+
+    // Set the world matrix
+    D3DXMATRIX matIdentity;
+    D3DXMatrixIdentity( &matIdentity );
+    m_pd3dDevice->SetTransform( D3DTS_WORLD,  &matIdentity );
+
+    // Set up our view matrix. A view matrix can be defined given an eye point,
+    // a point to lookat, and a direction for which way is up. Here, we set the
+    // eye five units back along the z-axis and up three units, look at the
+    // origin, and define "up" to be in the y-direction.
+    D3DXMATRIX matView;
+    D3DXVECTOR3 vFromPt   = D3DXVECTOR3( 0.0f, 0.0f, -5.0f );
+    D3DXVECTOR3 vLookatPt = D3DXVECTOR3( 0.0f, 0.0f, 0.0f );
+    D3DXVECTOR3 vUpVec    = D3DXVECTOR3( 0.0f, 1.0f, 0.0f );
+    D3DXMatrixLookAtLH( &matView, &vFromPt, &vLookatPt, &vUpVec );
+    m_pd3dDevice->SetTransform( D3DTS_VIEW, &matView );
+
+    // Set the projection matrix
+    D3DXMATRIX matProj;
+    FLOAT fAspect = ((FLOAT)m_d3dsdBackBuffer.Width) / m_d3dsdBackBuffer.Height;
+    D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI/4, fAspect, 1.0f, 100.0f );
+    m_pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+
+    // Set up lighting states
+    D3DLIGHT8 light;
+    D3DUtil_InitLight( light, D3DLIGHT_DIRECTIONAL, -1.0f, -1.0f, 2.0f );
+    m_pd3dDevice->SetLight( 0, &light );
+    m_pd3dDevice->LightEnable( 0, TRUE );
+    m_pd3dDevice->SetRenderState( D3DRS_LIGHTING, TRUE );
+
+    // Restore the font
+    m_pFont->RestoreDeviceObjects();
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: FrameMove()
+// Desc: Called once per frame, the call is the entry point for animating
+//       the scene.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::FrameMove()
+{
+    // TODO: update world
+
+    // Update user input state
+    UpdateInput( &m_UserInput );
+
+    // Update the world state according to user input
+    D3DXMATRIX matWorld;
+    D3DXMATRIX matRotY;
+    D3DXMATRIX matRotX;
+
+    if( m_UserInput.bRotateLeft && !m_UserInput.bRotateRight )
+        m_fWorldRotY += m_fElapsedTime;
+    else if( m_UserInput.bRotateRight && !m_UserInput.bRotateLeft )
+        m_fWorldRotY -= m_fElapsedTime;
+
+    if( m_UserInput.bRotateUp && !m_UserInput.bRotateDown )
+        m_fWorldRotX += m_fElapsedTime;
+    else if( m_UserInput.bRotateDown && !m_UserInput.bRotateUp )
+        m_fWorldRotX -= m_fElapsedTime;
+
+    D3DXMatrixRotationX( &matRotX, m_fWorldRotX );
+    D3DXMatrixRotationY( &matRotY, m_fWorldRotY );
+
+    D3DXMatrixMultiply( &matWorld, &matRotX, &matRotY );
+    m_pd3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: UpdateInput()
+// Desc: Update the user input.  Called once per frame 
+//-----------------------------------------------------------------------------
+void CMyD3DApplication::UpdateInput( UserInput* pUserInput )
+{
+    pUserInput->bRotateUp    = ( m_bHasFocus && (GetAsyncKeyState( VK_UP )    & 0x8000) == 0x8000 );
+    pUserInput->bRotateDown  = ( m_bHasFocus && (GetAsyncKeyState( VK_DOWN )  & 0x8000) == 0x8000 );
+    pUserInput->bRotateLeft  = ( m_bHasFocus && (GetAsyncKeyState( VK_LEFT )  & 0x8000) == 0x8000 );
+    pUserInput->bRotateRight = ( m_bHasFocus && (GetAsyncKeyState( VK_RIGHT ) & 0x8000) == 0x8000 );
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: Render()
+// Desc: Called once per frame, the call is the entry point for 3d
+//       rendering. This function sets up render states, clears the
+//       viewport, and renders the scene.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::Render()
+{
+    // Clear the viewport
+    m_pd3dDevice->Clear( 0L, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER,
+                         0x000000ff, 1.0f, 0L );
+
+    // Begin the scene
+    if( SUCCEEDED( m_pd3dDevice->BeginScene() ) )
+    {
+        // TODO: render world
+        
+        // Render the teapot mesh
+        m_pD3DXMesh->DrawSubset(0);
+
+        // Render stats and help text  
+        RenderText();
+
+        // End the scene.
+        m_pd3dDevice->EndScene();
+    }
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: RenderText()
+// Desc: Renders stats and help text to the scene.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::RenderText()
+{
+    D3DCOLOR fontColor        = D3DCOLOR_ARGB(255,255,255,0);
+    D3DCOLOR fontWarningColor = D3DCOLOR_ARGB(255,0,255,255);
+    TCHAR szMsg[MAX_PATH] = TEXT("");
+
+    // Output display stats
+    FLOAT fNextLine = 40.0f; 
+
+    lstrcpy( szMsg, m_strDeviceStats );
+    fNextLine -= 20.0f;
+    m_pFont->DrawText( 2, fNextLine, fontColor, szMsg );
+
+    lstrcpy( szMsg, m_strFrameStats );
+    fNextLine -= 20.0f;
+    m_pFont->DrawText( 2, fNextLine, fontColor, szMsg );
+
+    // Output statistics & help
+    fNextLine = (FLOAT) m_d3dsdBackBuffer.Height; 
+
+    wsprintf( szMsg, TEXT("Arrow keys: Up=%d Down=%d Left=%d Right=%d"), 
+              m_UserInput.bRotateUp, m_UserInput.bRotateDown, m_UserInput.bRotateLeft, m_UserInput.bRotateRight );
+    fNextLine -= 20.0f; m_pFont->DrawText( 2, fNextLine, fontColor, szMsg );
+
+    lstrcpy( szMsg, TEXT("Use arrow keys to rotate object") );
+    fNextLine -= 20.0f; m_pFont->DrawText( 2, fNextLine, fontColor, szMsg );
+
+    lstrcpy( szMsg, TEXT("Press 'F2' to configure display") );
+    fNextLine -= 20.0f; m_pFont->DrawText( 2, fNextLine, fontColor, szMsg );
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: MsgProc()
+// Desc: Overrrides the main WndProc, so the sample can do custom message
+//       handling (e.g. processing mouse, keyboard, or menu commands).
+//-----------------------------------------------------------------------------
+LRESULT CMyD3DApplication::MsgProc( HWND hWnd, UINT msg, WPARAM wParam,
+                                    LPARAM lParam )
+{
+    switch( msg )
+    {
+        case WM_PAINT:
+        {
+            if( m_bLoadingApp )
+            {
+                // Draw on the window tell the user that the app is loading
+                // TODO: change as needed
+                HDC hDC = GetDC( hWnd );
+                TCHAR strMsg[MAX_PATH];
+                wsprintf( strMsg, TEXT("Loading... Please wait") );
+                RECT rct;
+                GetClientRect( hWnd, &rct );
+                DrawText( hDC, strMsg, -1, &rct, DT_CENTER|DT_VCENTER|DT_SINGLELINE );
+                ReleaseDC( hWnd, hDC );
+            }
+            break;
+        }
+
+    }
+
+    return CD3DApplication::MsgProc( hWnd, msg, wParam, lParam );
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: InvalidateDeviceObjects()
+// Desc: Invalidates device objects.  
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::InvalidateDeviceObjects()
+{
+    // TODO: Cleanup any objects created in RestoreDeviceObjects()
+    m_pFont->InvalidateDeviceObjects();
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: DeleteDeviceObjects()
+// Desc: Called when the app is exiting, or the device is being changed,
+//       this function deletes any device dependent objects.  
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::DeleteDeviceObjects()
+{
+    // TODO: Cleanup any objects created in InitDeviceObjects()
+    m_pFont->DeleteDeviceObjects();
+    SAFE_RELEASE( m_pD3DXMesh );
+
+    return S_OK;
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+// Name: FinalCleanup()
+// Desc: Called before the app exits, this function gives the app the chance
+//       to cleanup after itself.
+//-----------------------------------------------------------------------------
+HRESULT CMyD3DApplication::FinalCleanup()
+{
+    // TODO: Perform any final cleanup needed
+    // Cleanup D3D font
+    SAFE_DELETE( m_pFont );
+
+    // Write the settings to the registry
+    WriteSettings();
+
+    return S_OK;
+}
+
+
+
+
